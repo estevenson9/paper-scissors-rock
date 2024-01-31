828e3e08abd2bb8c82bc914fbc961299ec9d7d27
estevenson <149737134+estevenson9@users.noreply.github.com>
2024-01-31 14:11:03 +1000

    Fixed buttons to play a single round, Add logging to html

--- gameloop.js
+++ gameloop.js
@@ -1,13 +1,28 @@
 "use strict";
-
+const possibleResults = new Map();
 const optionBtns = document.querySelectorAll(".options");
+const bodyElement = document.querySelector(".body");
+let score = [0, 0];
 optionBtns.forEach((element) => {
-  element.addEventListener("click", () => {
-    let outputResults = document.createElement("div");
-
-    console.log(element.value);
-  });
+  element.addEventListener("click", playRound);
 });
+
+const keys = [
+  ["scissors", "rock"],
+  ["scissors", "paper"],
+  ["rock", "paper"], //ensure these are ordered correctly so map is created with correct key value pairs
+  ["rock", "scissors"],
+  ["paper", "scissors"],
+  ["paper", "rock"],
+];
+for (let index = 0; index < keys.length; index++) {
+  const values = [0, 1];
+  if (index % 2) {
+    possibleResults.set(keys[index], values.reverse());
+  } else {
+    possibleResults.set(keys[index], values);
+  }
+}
 
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
@@ -25,24 +40,6 @@ function getComputerChoice() {
 }
 
 function gameplayLogic(playerInput, computerInput) {
-  const possibleResults = new Map();
-  const keys = [
-    ["scissors", "rock"],
-    ["scissors", "paper"],
-    ["rock", "paper"], //ensure these are ordered correctly so map is created with correct key value pairs
-    ["rock", "scissors"],
-    ["paper", "scissors"],
-    ["paper", "rock"],
-  ];
-  for (let index = 0; index < keys.length; index++) {
-    const values = [0, 1];
-    if (index % 2) {
-      possibleResults.set(keys[index], values.reverse());
-    } else {
-      possibleResults.set(keys[index], values);
-    }
-  }
-
   if (playerInput === computerInput) {
     return [0, 0];
   } else {
@@ -59,20 +56,22 @@ function gameplayLogic(playerInput, computerInput) {
     return possibleResults.get(checkResult);
   }
 }
-
-function gameplayLoop() {
-  let playerVal = "";
-  let score = [0, 0];
-  while (true) {
-    let computerVal = getComputerChoice();
-
-    console.log(`Computer chose ${computerVal}`);
-    let results = gameplayLogic(playerVal, computerVal);
-    for (let index = 0; index < score.length; index++) {
-      score[index] += results[index];
-    }
-    console.log(`Player - ${score[0]} Computer - ${score[1]}`);
-  }
+function playRound() {
+  return gameplayLoop(this.value);
 }
-
-gameplayLoop();
+function gameplayLoop(input) {
+  let playerVal = input;
+  let computerVal = getComputerChoice();
+  let resultsContainer = document.createElement("div");
+  let chosenChoices = document.createElement("p");
+  let roundResults = document.createElement("p");
+  chosenChoices.textContent = `Player chose ${playerVal}, Computer chose ${computerVal}`;
+  let results = gameplayLogic(playerVal, computerVal);
+  for (let index = 0; index < score.length; index++) {
+    score[index] += results[index];
+  }
+  roundResults.textContent = `Player - ${score[0]} Computer - ${score[1]}`;
+  resultsContainer.appendChild(chosenChoices);
+  resultsContainer.appendChild(roundResults);
+  bodyElement.appendChild(resultsContainer);
+}
